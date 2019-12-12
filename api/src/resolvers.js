e3329d20d490f43b2d87e3ccd568369bdf7d7b19
Aditya Agarwal <adityaagarwal.cse20@jecrc.ac.in>
2019-12-13 00:04:15 +0530

    add basic types and resolvers

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,19 @@
 
 module.exports = {
   Query: {
-    
+    pets(_, __, ctx) {
+      return ctx.models.Pet.findMany()
+    },
   },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
-  },
-  User: {
-    
-  }
+  // Mutation: {
+
+  // // },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300'
+  //   },
+  // },
+  // User: {},
 }
